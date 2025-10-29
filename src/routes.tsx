f6d34a2fa987a78ba71b84a057d6b7c3d3198587
mjay <michael.a.jay@protonmail.com>
2025-10-29 08:19:05 -0400

    add signup stub

--- src/routes.tsx
+++ src/routes.tsx
@@ -5,8 +5,10 @@ import Home from './pages/Home';
 import Login from './pages/auth/Login';
 import AcceptInvite from './pages/auth/AcceptInvite';
 import ResetPassword from './pages/auth/ResetPassword';
+import Signup from './pages/auth/Signup';
 
 export const router = createBrowserRouter([
+  { path: '/signup', element: <Signup /> },
   { path: '/login', element: <Login /> },
   { path: '/invite/accept', element: <AcceptInvite /> },
   { path: '/password/reset', element: <ResetPassword /> },
